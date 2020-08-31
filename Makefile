f18a021ac448bccdd7008315354811376ee4372c
Mark Yen <mark.yen@suse.com>
2020-08-31 16:15:22 -0700

    fix: address review feedback
    - Use kubecf-tools to determine the version, instead of `git describe`
      (directly, anyway)
    - Drop helm chart handling; assume the user will handle that in some other
      way (i.e. supply their own Kubernetes bits, which is the case for KubeCF)
    
    https://github.com/SUSE/eirini-dns-aliases/pull/1#pullrequestreview-472910075

--- Makefile
+++ Makefile
@@ -1,10 +1,7 @@
 .PHONY: binary image
 
-binary:
-	ruby build/binary.rb
-
 image:
 	ruby build/image.rb
 
-chart:
-	ruby build/chart.rb
+binary:
+	ruby build/binary.rb
--- build/binary.rb
+++ build/binary.rb
@@ -6,7 +6,7 @@ manifest = load_manifest
 output_path = File.join(manifest.binary.output.directory,
                         manifest.binary.output.name)
 
-version = `git describe --tag --always --long`
+version = ENV['VERSION'] || `ruby build/kubecf-tools/versioning/versioning.rb`
 
 exec({ 'GOOS' => 'linux', 'GOARCH' => 'amd64' },
      'go', 'build', "-ldflags=-s -w -X main.appVersion=#{version}",
